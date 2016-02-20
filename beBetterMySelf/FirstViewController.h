bfbd8321a9be21eec55f181a5634d9b3d3ac074e
wujianpeng <wjp@xkeshi.com>
2016-02-20 21:01:20 +0800

    Initial Commit

---
+++ beBetterMySelf/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  beBetterMySelf
+//
+//  Created by 肖敏 on 16/2/20.
+//  Copyright © 2016年 xiaomin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ beBetterMySelf/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  beBetterMySelf
+//
+//  Created by 肖敏 on 16/2/20.
+//  Copyright © 2016年 xiaomin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
